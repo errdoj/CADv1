a1930e01e047dd8f14cc41b48d7a0a5bf79da255
errdoj <67370988+errdoj@users.noreply.github.com>
2020-07-19 09:20:52 -0700

    Files

---
+++ Codes10.jsx
@@ -0,0 +1,252 @@
+import React, { Component } from 'react';
+
+export default class Codes10 extends Component {
+  render() {
+    return (
+      <div className='container-fluid pb-5 text-light'>
+        <div className='d-flex justify-content-between'>
+          <h2>10 Codes</h2>
+          <a href='/leo/dash' className='btn btn-secondary mb-2'>
+            Back To Dashboard
+          </a>
+        </div>
+        <div class='row'>
+          <div class='col-md-3'>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Service Status:
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-5: Meal Break<br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-7: Out of Service<br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-8: In Service<br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-12: Active Ride Along <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-15: Suspect in Custody
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-23: Arrived on Scene <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-41: Beginning tour of duty <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-42: Ending tour of duty <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-65: Escorting Prisoner <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-93: Removed from game <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-97: En route <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Code 0: Game crash <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Code 1: No Lights nor sirens <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Code 2: Lights, no sirens <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Code 3: Lights and Sirens
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Signal 11: Running Radar
+            </li>
+          </div>
+          <div class='col-md-3'>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Scenes:
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-10: Fight in progress <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-11: Traffic Stop <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-13: Shots Fired <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-16: Stolen Vehicle <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-17: Suspicious Vehicle <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-25: Domestic Dispute <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-49: Homicide <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-50: Vehicle Accident <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-62: Kidnapping <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-64: Sexual Assault <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-66: Reckless Driver <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-67: Fire <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-68: Armed Robbery <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-70: Foot Pursuit <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-80: Vehicle Pursuit <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-99: Officer in Distress <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Code 5: Felony Stop <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Signal 11: Radar (Speed Trap)
+            </li>
+          </div>
+          <div class='col-md-3'>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Requests:
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-27: Drivers License Check <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-28: Plate Check <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-29: Warrant Check <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-32: Requesting Backup <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-51: Towing Service <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-52: Requesting EMS <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-53: Requesting Fire Dept. <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-71: Supervisor Needed
+            </li>
+          </div>
+          <div class='col-md-3'>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Subject Status:
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-0: Disappeared <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-30: Wanted Person <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-55: Intoxicated Driver <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-60: Armed With a Gun <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-44: Person Deceased <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-15: Subject in Custody <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-31: Not wanted, No warrants <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-56: Intoxicated Pedestrian <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-61: Armed with a knife
+            </li>
+          </div>
+          <div class='col-md-3'>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Radio Comms:
+            </li>
+
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-1: Frequency Change <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              {' '}
+              10-2: Radio Check <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              {' '}
+              10-3: Stop Transmitting <br />
+            </li>
+
+            <li class='list-group-item bg-dark border-dark text-light'>
+              {' '}
+              10-4: Affirmative <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-6: Busy <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              {' '}
+              10-9: Repeat <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              {' '}
+              10-22: Disregard <br />
+            </li>
+
+            <li class='list-group-item bg-dark border-dark text-light'>
+              {' '}
+              10-26: ETA <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-35: Wrap up the scene <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-43: Information <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              10-73: Advise Status <br />
+            </li>
+
+            <li class='list-group-item bg-dark border-dark text-light'>
+              {' '}
+              10-90: In-Game Warning <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Code 4: Under control <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Signal 37: Meet at Location <br />
+            </li>
+            <li class='list-group-item bg-dark border-dark text-light'>
+              Signal 100: Emergency Ttraffic
+            </li>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ PenalCodes.jsx
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import LoadingArea from '../Partials/LoadingArea';
+import Axios from 'axios';
+import { backendURL } from '../../config/config';
+import Cookies from 'js-cookie';
+import ErrorMessage from '../Partials/Messages/ErrorMessage';
+import ReactMarkdown from 'react-markdown';
+
+export default class PenalCodes extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      penalCodes: [],
+      filteredPenalCodes: [],
+      loading: true,
+    };
+  }
+
+  getPenalCodes = () => {
+    Axios({
+      url: backendURL + '/officers/penal-codes',
+      headers: {
+        'x-auth-snailycad-token': Cookies.get('__session'),
+      },
+    })
+      .then((res) => {
+        if (res.data.penalCodes) {
+          this.setState({
+            penalCodes: res.data.penalCodes,
+            filteredPenalCodes: res.data.penalCodes,
+            loading: false,
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  componentDidMount() {
+    document.title = 'Penal Codes - LEO';
+    this.getPenalCodes();
+  }
+
+  handleSearch = (e) => {
+    // Thanks to: https://codepen.io/iamtimsmith/pen/zJPzwN?editors=1010
+    let currentList = [];
+    let newList = [];
+
+    if (e.target.value === '') {
+      newList = this.state.penalCodes;
+    } else {
+      currentList = this.state.penalCodes;
+
+      newList = currentList.filter((item) => {
+        const lc = item.title.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lc.includes(filter);
+      });
+    }
+
+    this.setState({
+      filteredPenalCodes: newList,
+    });
+  };
+
+  render() {
+    const { filteredPenalCodes, loading } = this.state;
+
+    if (loading) return <LoadingArea />;
+
+    return (
+      <div className='container-fluid mt-3 text-light'>
+        <div className="d-flex justify-content-between mb-2">
+        <h3>Penal Codes</h3>
+        <a href="/leo/dash" className="btn btn-secondary">Back to Dashboard</a>
+        </div>
+        <ul className='list-group'>
+          <input
+            type='search'
+            name='penalCode'
+            placeholder='Search By Title'
+            onChange={this.handleSearch}
+            className='form-control bg-dark border-secondary text-light mb-3'
+          />
+          {!filteredPenalCodes[0] ? (
+            <ErrorMessage message='No Penal Codes found with that title' />
+          ) : (
+            filteredPenalCodes.map((code, index) => {
+              return (
+                <li
+                  key={index}
+                  className='list-group-item bg-dark border-secondary'>
+                  <h4 className='font-weight-bold'> {code.title} </h4>
+                  <ReactMarkdown escapeHtml={false} source={code.des} />
+                </li>
+              );
+            })
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
